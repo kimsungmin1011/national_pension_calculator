fd3c2c1a2357dfde1c96df2ebc1f09b60e4dcff3
kimsungmin1011 <tata153@naver.com>
2023-05-21 14:12:50 +0900

    가입월수 -> 가입연수 변경

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ document.getElementById('pensionForm').addEventListener('submit', function (even
   var X = 1.275;
   var B = parseFloat(document.getElementById('B').value);
   var n = parseFloat(document.getElementById('n').value);
+
   if (B >= 5900000) {
     B = 5900000
   } else if (B <= 370000) {
     B = 370000
   }
 
-  var pension = X * (A + B) * (1 + 0.05 * n / 12) / 12;
+  var pension = X * (A + B) * (1 + 0.05 * n) / 12;
 
   document.getElementById('result').innerText = (pension.toFixed(0)).toLocaleString('ko-KR') + '원';
 });
