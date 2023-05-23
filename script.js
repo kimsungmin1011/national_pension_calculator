7a96ca91cf294a968489c53ee59ef73175aa46c4
김성민 <sungminkim@gimseongmin-ui-MacBookPro.local>
2023-05-23 17:31:44 +0900

    가입기간이 여러 구간에 걸쳐있는 경우의 연금액 산식 추가

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.getElementById('pensionForm').addEventListener('submit', function (event) {
   event.preventDefault();
-  
+
   var Y = parseFloat(document.getElementById('Y').value);
   var A = 2816091;
-  var X = 1.2;
+  var X = 1.25;
   var B = parseFloat(document.getElementById('B').value) * 10000;
-  var n = parseFloat(document.getElementById('n').value);
   var n = parseFloat(document.getElementById('n').value);
   var year = parseInt(document.getElementById('year').value);
 
@@ -15,27 +14,27 @@ document.getElementById('pensionForm').addEventListener('submit', function (even
     B = 370000
   }
 
-  if (year>=1988 && year <= 1998){
-    var pension = 2.4 * (A + 0.75 * B) * (1 + 0.05 * n) / 12;
-  } else if (year >= 1999 && year <=2007){
-    pension = 1.8 * (A + B) * (1 + 0.05 * n) / 12;
-  } else if (year >=2008) {
-    pension = X * (A + B) * (1 + 0.05 * n) / 12;
+  if (year >= 1988 && year <= 1998) {
+    var pension = (2.4 * (A + 0.75 * B) * (1 + 0.05 * n) / 12)*((1998-year+1)/(n+20)) + (1.8 * (A + B) * (1 + 0.05 * n) / 12) * ((2007-1999+1)/(n+20)) + (X * (A + B) * (1 + 0.05 * n) / 12) * ((year+n+20-2008)/(n+20));
+  } else if (year >= 1999 && year <= 2007) {
+    pension = (1.8 * (A + B) * (1 + 0.05 * n) / 12) * ((2007-year+1)/(n+20)) + (X * (A + B) * (1 + 0.05 * n) / 12) * ((year+n+20-2008)/(n+20));
+  } else if (year >= 2008) {
+    pension = (X * (A + B) * (1 + 0.05 * n) / 12);
   } else {
     alert("국민연금제도는 1988년부터 시행되었습니다");
   }
 
-  if (Y<=1952){
+  if (Y <= 1952) {
     var SY = Y + 60;
-  }else if (Y>=1953 && Y<=1956){
+  } else if (Y >= 1953 && Y <= 1956) {
     SY = Y + 61;
-  }else if (Y>=1957 && Y<=1960){
+  } else if (Y >= 1957 && Y <= 1960) {
     SY = Y + 62;
-  }else if (Y>=1961 && Y<=1964){
+  } else if (Y >= 1961 && Y <= 1964) {
     SY = Y + 63;
-  }else if (Y>=1965 && Y<=1968){
+  } else if (Y >= 1965 && Y <= 1968) {
     SY = Y + 64;
-  }else if (Y>=1969){
+  } else if (Y >= 1969) {
     SY = Y + 65;
   }
 
@@ -43,5 +42,5 @@ document.getElementById('pensionForm').addEventListener('submit', function (even
   document.getElementById('total').innerText = B * 0.09 * 12 * (20 + n) + '원';
   document.getElementById('result').innerText = (pension.toFixed(0)).toLocaleString('ko-KR') + '원';
   document.getElementById('age').innerText = SY + '년';
-  document.getElementById('sonik').innerText =  Math.floor((B * 0.09 * 12 * (20 + n)) / (pension * 12))+ Y + 60 + '년';
+  document.getElementById('sonik').innerText = Math.floor((B * 0.09 * 12 * (20 + n)) / (pension * 12)) + SY + '년';
 });
